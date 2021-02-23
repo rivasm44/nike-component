eb95d6863ee5f75fa1e3f29fe35e605e46158b51
OMAR RIVAS <omarrivas@MacBook-Pro-de-OMAR.local>
2021-02-23 16:32:08 -0600

    Probando el componente...

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1091891.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 42637644, 42637648, 42637647, 42637645, 42640791, 42697175, 42764044, 42777529 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
